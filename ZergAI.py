1f58aab25bfbe8bc2678c85b1b6de4126003d610
Sérgio Sacramento <sergiosacramentosouza@gmail.com>
2018-10-15 20:57:17 -0300

    Adicionando estratégia para a AI construir novos workers.

--- ZergAI.py
+++ ZergAI.py
@@ -1,10 +1,17 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import LARVA, DRONE
 
 class ZergAI(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
+        await self.build_workers()
+
+    async def build_workers(self):
+        larvae = self.units(LARVA)
+        if self.can_afford(DRONE) and larvae.exists:
+            await self.do(larvae.random.train(DRONE))
 
 
 run_game(maps.get("AbyssalReefLE"), [
